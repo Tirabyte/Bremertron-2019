4f639e598fca0d403166ad3b6505e1e5bbef6a76
Jeremy Tirador <tear_a_door@Jeremys-MacBook-Air.local>
2019-01-10 14:47:47 -0800

    Added Joystick Buttons

--- knightsInSpace2019/src/main/java/frc/robot/OI.java
+++ knightsInSpace2019/src/main/java/frc/robot/OI.java
@@ -7,22 +7,57 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  public static Joystick joy = new Joystick(1); // Drive Joystick
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+	//Drive Stick Button Mapping
+	JoystickButton button1 = new JoystickButton(joy, 1);
+	JoystickButton button2 = new JoystickButton(joy, 2);
+	JoystickButton button3 = new JoystickButton(joy, 3);
+	JoystickButton button4 = new JoystickButton(joy, 4);
+	JoystickButton button5 = new JoystickButton(joy, 5);
+	JoystickButton button6 = new JoystickButton(joy, 6);
+	JoystickButton button7 = new JoystickButton(joy, 7);
+	JoystickButton button8 = new JoystickButton(joy, 8);
+	JoystickButton button9 = new JoystickButton(joy, 9);
+	JoystickButton button10 = new JoystickButton(joy, 10);
+	JoystickButton button11 = new JoystickButton(joy, 11);
+	public static int axisX = 0;
+	public static int axisY = 1;
+	public static int axisZ = 2;
+
+	public static Joystick aux = new Joystick(0); // Auxilary Joystick
+
+	//Auxilary Stick Button Mapping
+	JoystickButton buttonAux1 = new JoystickButton(aux, 1);
+	JoystickButton buttonAux2 = new JoystickButton(aux, 2);
+	JoystickButton buttonAux3 = new JoystickButton(aux, 3);
+	JoystickButton buttonAux4 = new JoystickButton(aux, 4);
+	JoystickButton buttonAux5 = new JoystickButton(aux, 5);
+	JoystickButton buttonAux6 = new JoystickButton(aux, 6);
+	JoystickButton buttonAux7 = new JoystickButton(aux, 7);
+	JoystickButton buttonAux8 = new JoystickButton(aux, 8);
+	JoystickButton buttonAux9 = new JoystickButton(aux, 9);
+	JoystickButton buttonAux10 = new JoystickButton(aux, 10);
+	JoystickButton buttonAux11 = new JoystickButton(aux, 11);
+	public static int axisAuxX = 0;
+	public static int axisAuxY = 1;
+  public static int axisAuxZ = 2;
+
+  public OI(){
+
+  }
+
+  public Joystick getJoystick(){
+    return joy;
+  }
 
   //// TRIGGERING COMMANDS WITH BUTTONS
   // Once you have a button, it's trivial to bind it to a button in one of
